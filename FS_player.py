b929985ff2297108fc6afa452c1e240b3145564b
Sebastian1812 <115327871+Sebastian1812@users.noreply.github.com>
2022-12-11 05:47:55 -0800

    PY_Contemos 11_12_2022_05:47am

--- FS_player.py
+++ FS_player.py
@@ -5,20 +5,24 @@ class Gato(pygame.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
         self.sheet = pygame.image.load("Imagenes/Gato/spriteSheet.png")
-        self.sheet.set_clip(pygame.Rect(0, 0, 37, 32))
+        self.sheet.set_clip(pygame.Rect(0, 0, 280, 200))
         self.image = self.sheet.subsurface(self.sheet.get_clip())
         self.rect = self.image.get_rect()
         self.rect = self.image.get_rect()
         self.rect.topleft = position
         self.frame = 0
-        self.right_states = {0: (0, 0, 37, 32),
-                             1: (37, 0, 37, 32),
-                             2: (74, 0, 37, 32),
-                             3: (111, 0, 37, 32),
-                             4: (148, 0, 37, 32),
-                             5: (185, 0, 37, 32),
-                             6: (222, 0, 37, 32),
-                             7: (259, 0, 37, 32)}
+        self.right_states = {0: (0, 0, 280, 200),
+                             1: (295, 0, 202, 200),
+                             2: (581, 0, 210, 200),
+                             3: (830, 0, 254, 200),
+                             4: (1124, 0, 260, 200),
+                             5: (1410, 0, 250, 200),
+                             6: (1707, 0, 250, 200),
+                             7: (2015, 0, 227, 200)}
+
+
+
+
 
     def get_frame(self, frame_set):
         self.frame += 1
@@ -34,17 +38,24 @@ class Gato(pygame.sprite.Sprite):
         return clipped_rect
 
     def update(self, direction):
-        if direction == "right":
+        if direction == 'left':
+            self.clip(self.left_states)
+            self.rect.x -= 5
+        if direction == 'right':
             self.clip(self.right_states)
-            self.rect.x += 183
-        if direction == "stand_right":
-            self.clip(self.right_states[0])
-        self.image = self.sheet.subsurface(self.sheet.get_clip())
+            self.rect.x += 5
+        if direction == 'up':
+            self.clip(self.up_states)
+            self.rect.y -= 5
+        if direction == 'down':
+            self.clip(self.down_states)
+            self.rect.y += 5
+
 
     def handle_event(self, evento):
         if evento.type == pygame.KEYDOWN:
             if evento.type == pygame.K_RIGHT:
-                self.update("right")
+                self.update('right')
         if evento.type == pygame.KEYUP:
             if evento.type == pygame.K_RIGHT:
-                self.update("stand_right")
+                self.update('stand_right')
