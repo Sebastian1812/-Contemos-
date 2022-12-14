59644185e52cdd2d8796af6e8e29b135d233bb74
Sebastian1812 <115327871+Sebastian1812@users.noreply.github.com>
2022-12-14 07:11:09 -0800

    Add files via upload

--- FS_button.py
+++ FS_button.py
@@ -1,25 +1,25 @@
-from pygame import*
+from pygame import *
 
+#S O N I D O
 mixer.init()
 soundbot = mixer.Sound("Musica/boton.wav")
 
-class Button():
+
+class Button:
     def __init__(self, x, y, imagenes):
-        self.image = imagenes[0]
-        self.imagetwo = imagenes[1]
-        self.imagebot = imagenes[2]
-        self.rect = self.image.get_rect()
+        self.image = imagenes
+        self.rect = self.image[0].get_rect()
         self.rect.topleft = (x, y)
         self.clicked = False
 
     def draw(self, root):
         action = False
-        root.blit(self.image, (self.rect.x, self.rect.y))
+        root.blit(self.image[0], (self.rect.x, self.rect.y))
         pos = mouse.get_pos()
         if self.rect.collidepoint(pos):
-            root.blit(self.imagetwo, (self.rect.x, self.rect.y))
-            if mouse.get_pressed()[0] == 1 and self.clicked == False:
-                root.blit(self.imagebot, (self.rect.x, self.rect.y))
+            root.blit(self.image[1], (self.rect.x, self.rect.y))
+            if mouse.get_pressed()[0] == 1 and not self.clicked:
+                root.blit(self.image[2], (self.rect.x, self.rect.y))
                 mixer.Sound.play(soundbot)
                 self.clicked = True
                 action = True
--- FS_player.py
+++ FS_player.py
@@ -1,21 +1,28 @@
 from pygame import *
 from random import *
-from FS_button import *
 
+init()
+
+#V A R I A B L E S
 bg = 1
 xbackground = 0
 n1 = 0
 n2 = 0
-xtotal = 0
+res = 0
+meta =50
 
-init()
+#S O N I D O
+mixer.init()
+saltoson = mixer.Sound("Musica/Salto_sonido.wav")
+correcto = mixer.Sound("Musica/correcto.wav")
+incorrecto = mixer.Sound("Musica/incorrecto.wav")
 
 # C O L O R E S
 naranja = Color(212, 139, 11)
 negro = Color(0, 0, 0)
 
 # F U E N T E S
-font1 = font.SysFont('Fuentes/Golden Age Shad', 80)
+font1 = font.Font('Fuentes/Golden Age Shad.ttf', 60)
 
 # I M A G E N E S
 life_bar = [image.load("Imagenes/Gato/Lifes/0.png"),
@@ -27,29 +34,31 @@ life_bar = [image.load("Imagenes/Gato/Lifes/0.png"),
             image.load("Imagenes/Gato/Lifes/6.png"),
             image.load("Imagenes/Gato/Lifes/7.png")]
 
+
 def resultado(n1, n2, resul):
     check = n1 + n2
     puntos = 10
     if resul == check:
-        print("CORRECTO")
+        mixer.Sound.play(correcto)
         return True
     else:
-        print("INCORRECTO")
+        mixer.Sound.play(incorrecto)
         return False
 
 
 class Gato(sprite.Sprite):
-    def __init__(self, position, x):
+    def __init__(self, position):
         super().__init__()
         self.sheet = image.load("Imagenes/Gato/spriteSheet.png")
         self.sheet.set_clip(Rect(830, 0, 254, 200))
         self.image = self.sheet.subsurface(self.sheet.get_clip())
         self.rect = self.image.get_rect()
-        self.rect = self.image.get_rect()
         self.rect.topleft = position
         self.resul = 1
         self.puntos = 0
-        self.vidas = 3
+        self.vidas = 7
+        self.game_over = False
+        self.game_restart = False
         self.problem = True
         self.frame = 0
         self.right_states = {0: (830, 0, 254, 200),
@@ -76,21 +85,52 @@ class Gato(sprite.Sprite):
         return clipped_rect
 
 
-    def retry(self, x, root):
-        global bg, xbackground
+    def retry(self, x, root, band):
+        global bg, xbackground, res
         bg = 1
         self.resul = 1
         self.problem = True
         self.rect.x = x
         xbackground = x
-        self.puntos = 0
-        self.vidas -= 1
-        if self.vidas == 0:
-            self.vidas = 3
+        if band == 0:
+            self.vidas -= 1
+            if self.vidas == 0:
+                self.game_over = True
+        elif band == 1:
+            self.vidas = 7
+            self.puntos = 0
+            res = 0
+        elif band == 2:
+            self.puntos = self.puntos*self.vidas
+            res = 0
 
-    def handle_event(self, root, f1, evento, x):
-        global xbackground, bg, fond, n1, n2
 
+    def movimientos(self, x):
+        global xbackground, bg
+        if self.resul < 50:
+            mixer.Sound.play(saltoson)
+            self.clip(self.right_states)
+            self.rect.x += 200
+            self.resul += 1
+            xbackground += 200
+            if self.rect.x >= 946:
+                self.rect.x = x
+                bg += 1
+            if xbackground >= 1892:
+                xbackground = x
+
+    def handle_event(self, root, f1, evento, x, band):
+        global xbackground, bg, fond, n1, n2, res
+
+        #REVISA COMO VOLVERA A JUGAR EL USUARIO
+        if band == 1:
+            self.retry(x, root, 1)
+            self.game_over = False
+        elif band == 2:
+            self.retry(x, root, 2)
+            self.game_over = False
+
+        # CAMBIO DE ESCENARIO
         if xbackground < 948:
             root.blit(f1[0], (0, 0))
         else:
@@ -101,61 +141,51 @@ class Gato(sprite.Sprite):
         if self.problem:
             n1 = self.resul
             n2 = randrange(1, 10)
+            while n1+n2 > meta:
+                n2 = randrange(1, 10)
             self.problem = False
+
 
         # T E X T O S
         txt = [font1.render(str(n1), 1, negro),
                font1.render(str(n2), 1, negro),
-               font1.render(str(self.resul), 1, negro), ]
+               font1.render(str(self.resul), 1, negro)]
         points = font1.render(str(self.puntos), 1, negro)
 
+        # IMPRIMIR TEXTOS A LA VENTANA
         root.blit(txt[0], (270, 315))
         root.blit(txt[1], (543, 315))
         root.blit(txt[2], (815, 315))
-        root.blit(points, (200, 50))
+        root.blit(points, (50, 50))
 
-        if evento.type == KEYDOWN and self.buton == False:
-            if evento.key == K_RETURN:
-                if resultado(n1, n2, self.resul):
-                    self.puntos += 10
-                    self.problem = True
-                else:
-                    self.retry(x, root)
+        # MOVIMIENTO DE LAS FLECHAS
+        if not self.game_over:
+            if evento.type == KEYDOWN and self.buton == False:
+                if not self.buton:
+                    self.buton = True
+                    if evento.key == K_RIGHT:
+                        self.movimientos(x)
+                    elif evento.key == K_ESCAPE:
+                        quit()
+                        exit()
 
-            if self.buton == False:
-                self.buton = True
+                    elif evento.key == K_RETURN:
+                        if resultado(n1, n2, self.resul):
+                            self.puntos += 10
+                            res = self.resul
+                            if res == meta:
+                                self.game_over = True
+                            else:
+                                self.problem = True
+                        else:
+                            self.retry(x, root, 0)
+
+
+            if evento.type == KEYUP:
                 if evento.key == K_RIGHT:
-                    self.clip(self.right_states)
-                    self.rect.x += 200
-                    self.resul += 1
-                    xbackground += 200
-                    if self.rect.x >= 946:
-                        self.rect.x = x
-                        bg += 1
-                    if xbackground >= 1892:
-                        xbackground = x
-                elif evento.key == K_ESCAPE:
-                    quit()
-                    exit()
-
-            """elif evento.key == pygame.K_LEFT:
-                self.clip(self.right_states)
-                self.rect.x -= 183
-                xbackground -= 183
-                if self.rect.x < x:
-                    self.rect.x = x"""
-
-            # print(self.rect.x)
-
-        if evento.type == KEYUP:
-            if evento.key == K_RIGHT:
-                self.clip(self.right_states[0])
+                    self.clip(self.right_states[0])
                 self.buton = False
-            elif evento.key == K_RETURN:
-                self.buton = False
-            """elif evento.key == pygame.K_LEFT:
-                self.clip(self.right_states[0])"""
 
-        self.image = self.sheet.subsurface(self.sheet.get_clip())
+            self.image = self.sheet.subsurface(self.sheet.get_clip())
 
-        return bg
+        return bg, self.game_over, res, self.vidas, self.puntos
